80cc535e72120747501cf4807abf3ca4fcd7967c
KostasEreksonas <k.ereksonas@gmail.com>
2024-12-23 12:58:15 +0200

    Navigacija

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {AboutComponent} from './components/about/about.component';
+import {BenchInfoComponent} from './components/bench-info/bench-info.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"about", component:AboutComponent},
+  {path:"benchmarks", component:BenchInfoComponent}
+];
---
+++ src/app/components/bench-info/bench-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bench-info',
+  imports: [],
+  templateUrl: './bench-info.component.html',
+  styleUrl: './bench-info.component.css'
+})
+export class BenchInfoComponent {
+
+}
